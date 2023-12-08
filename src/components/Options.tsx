36b690fb9d175441d8fb221a06a043db49d79fb3
Noah Heath <noahadamheath@gmail.com>
2023-12-07 20:40:27 -0500

    level 3 maybe

--- src/components/Options.tsx
+++ src/components/Options.tsx
@@ -13,8 +13,7 @@ export const Options: React.FC<{ createGame: Function }> = ({ createGame }) => {
     const levels = [
         { value: 1, label: 1 },
         { value: 2, label: 2 },
-        { value: 3, label: 3 },
-        { value: 4, label: 4 }
+        { value: 3, label: 3 }
     ]
 
     function options(): JSX.Element {
