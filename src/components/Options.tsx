1ca6755b7e9b31e8cd4eb65042c29d1639ba08ec
Noah Heath <noahadamheath@gmail.com>
2023-12-10 12:49:32 -0500

    level 9

--- src/components/Options.tsx
+++ src/components/Options.tsx
@@ -17,8 +17,7 @@ export const Options: React.FC<{ createGame: Function }> = ({ createGame }) => {
         { value: 6, label: 6 },
         { value: 7, label: 7 },
         { value: 8, label: 8 },
-        { value: 9, label: 9 },
-        { value: 10, label: 10 }
+        { value: 9, label: 9 }
     ]
 
     function options(): JSX.Element {
